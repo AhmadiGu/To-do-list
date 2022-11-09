242ee23968e7591c54707bc8e63c8e8c7a135dc3
AhmadiGu <ahmadigulalai76@gmail.com>
2022-11-09 16:03:16 +0500

    fixed the errors

--- modules/add.js
+++ modules/add.js
@@ -1,5 +1,5 @@
 class Dom {
-  static add = (taskArray) => { 
+  static add = (taskArray) => {
     taskArray.forEach((element) => {
       taskArray += `<li><span><input type="checkbox" name="taskArray" value="${element.completed}"><input type="text"  name="taskArrayi" value="${element.description}"></span>
           <i class="fa-solid fa-ellipsis-vertical"></i></li>`;
