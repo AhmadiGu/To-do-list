91d22e91125474709866931a0f374f11702eae19
AhmadiGu <ahmadigulalai76@gmail.com>
2022-11-09 15:53:59 +0500

    Fixed the errors

--- modules/add.js
+++ modules/add.js
@@ -1,10 +1,8 @@
-import taskArray from './taskArray.js';
-
 class Dom {
   static add = (taskArray) => {
     //let taskArray = taskArray;
     taskArray.forEach((element) => {
-      taskArray += `<li><span><input type="checkbox" name="taskArray" value="${element.completed}"><input type="text"  name="taskArray" value="${element.description}"></span>
+      taskArray += `<li><span><input type="checkbox" name="taskArray" value="${element.completed}"><input type="text"  name="taskArrayi" value="${element.description}"></span>
           <i class="fa-solid fa-ellipsis-vertical"></i></li>`;
     });
     return taskArray;
