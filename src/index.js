39c483487d06710c706b772db3b70cf2d5fdb2d0
AhmadiGu <ahmadigulalai76@gmail.com>
2022-11-11 16:40:12 +0500

    Functionalties added

--- src/index.js
+++ src/index.js
@@ -1,8 +1,21 @@
-import Dom from '../modules/add';
-import taskArray from '../modules/taskArray';
+import DisplayTodos from './modules/functionalities.js';
 
-document.querySelector('.toDo-list').innerHTML = `
-<ul class="list">
-${Dom.add(taskArray)}
-</ul>
-`;
+window.addEventListener('load', () => {
+  const todos = JSON.parse(localStorage.getItem('todos')) || [];
+  const toDoInput = document.querySelector('.toDo-add-form');
+  toDoInput.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const todo = {
+      addList: e.target.elements.addList.value,
+      compeled: false,
+      createAt: new Date().getTime(),
+    };
+
+    todos.push(todo);
+    localStorage.setItem('todos', JSON.stringify(todos));
+
+    e.target.reset();
+    DisplayTodos();
+  });
+  DisplayTodos();
+});
